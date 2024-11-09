48576800d05130f966812b1c63c62ed48efb0fd7
12c12 <12c12@gmail.com>
2024-11-09 14:02:33 +0300

    .

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,25 @@
+import { router } from "expo-router";
+// import { useRef, useState } from "react";
+import {  Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const signup = () => {
+
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/welcome");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">hello</Text>
+      </TouchableOpacity>
+
+
+
+    </SafeAreaView>
+  );
+};
+
+export default signup;
